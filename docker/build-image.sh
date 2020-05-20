2247a439832b8f6d62e0130a46d640240db0b709
namgivu <namgivu@gmail.com>
2020-05-20 18:04:22 +0700

    reader friendly

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -2,5 +2,5 @@
 SCRIPT_HOME=`cd $(dirname $BASH_SOURCE) && pwd`  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
    APP_HOME=`cd "$SCRIPT_HOME/../" && pwd`
 
-docker build --file "$SCRIPT_HOME/Dockerfile" -t trangtruong1610/docker-action $APP_HOME && docker run -it trangtruong1610/docker-action
-
+docker build --file "$SCRIPT_HOME/Dockerfile"   -t trangtruong1610/docker-action   $APP_HOME       && docker run -it trangtruong1610/docker-action
+#            Dockerfile path                    image tag                          source folder      run the built image
