b62924c9416eeb3f3fb33326c1619ea14f063dc7
namgivu <namgivu@gmail.com>
2020-05-20 18:43:36 +0700

    clear before run

--- docker/build-n-run.sh
+++ docker/build-n-run.sh
@@ -2,5 +2,12 @@
 SCRIPT_HOME=`cd $(dirname $BASH_SOURCE) && pwd`  # get SCRIPT_HOME=executed script's path, containing folder, cd & pwd to get container path
    APP_HOME=`cd "$SCRIPT_HOME/../" && pwd`
 
-docker build --file "$SCRIPT_HOME/Dockerfile"   -t trangtruong1610/docker-action   $APP_HOME       && docker run -it trangtruong1610/docker-action
-#            Dockerfile path                    image tag                          source folder      run the built image
+it='trangtruong1610/docker-action'   # it aka image_tag
+cn='trangtruong1610-docker-action'   # cn aka container_name
+    # clear before run new
+    docker stop $cn || true; docker rm $cn || true   # remove image if exists
+    docker image rm $it || true   # remove image if exists
+
+    # cleared now, run it
+    docker build --file "$SCRIPT_HOME/Dockerfile"   -t $it      $APP_HOME       && docker run --name=$cn $it
+    #            Dockerfile path                    image tag   source folder      run the built image
