b25aaa69fb544889cffebf3f3c17f01a948bde7b
namgivu <namgivu@gmail.com>
2020-05-20 17:56:10 +0700

    nn review

--- src/test.py
+++ src/test.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 import os
 
-filepath = os.path.dirname(__file__)
-app_path = os.path.dirname(filepath)
+PWD      = os.path.dirname(__file__)
+APP_HOME = os.path.abspath(os.path.join(PWD, '..') )
 
-load_dotenv(dotenv_path=f'{app_path}/docker/config_vault/.env.DEV')
+load_dotenv(dotenv_path=f'{APP_HOME}/docker/config_vault/.env.DEV')
 
 ECHO_VAR = os.environ.get('ECHO_VAR')
 
-print(ECHO_VAR)
+print(f'APP_HOME={APP_HOME}')
+print(f'ECHO_VAR={ECHO_VAR}')
