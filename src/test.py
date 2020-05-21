f0c36b198038141875e9ad5b5ba8d37eca13a167
namgivu <namgivu@gmail.com>
2020-05-21 12:07:24 +0700

    GIT_COMMIT_ID as env var in container

--- src/test.py
+++ src/test.py
@@ -6,7 +6,11 @@ APP_HOME = os.path.abspath(os.path.join(PWD, '..') )
 
 load_dotenv(dotenv_path=f'{APP_HOME}/docker/config_vault/.env.DEV')
 
-ECHO_VAR = os.environ.get('ECHO_VAR')
+ECHO_VAR      = os.environ.get('ECHO_VAR')
+GIT_COMMIT_ID = os.environ.get('GIT_COMMIT_ID')
 
-print(f'APP_HOME={APP_HOME}')
-print(f'ECHO_VAR={ECHO_VAR}')
+print(f'''
+     APP_HOME={APP_HOME}
+     ECHO_VAR={ECHO_VAR}
+GIT_COMMIT_ID={GIT_COMMIT_ID}
+''')
