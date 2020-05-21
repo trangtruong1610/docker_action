bfd9d9f194b1e91565038a274901e38f87e16bea
Trang Truong <trang.truong@gigacover.com>
2020-05-21 10:43:04 +0700

    print git commit id

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-import os
+import os, subprocess
 
 PWD      = os.path.dirname(__file__)
 APP_HOME = os.path.abspath(os.path.join(PWD, '..') )
@@ -8,5 +8,8 @@ load_dotenv(dotenv_path=f'{APP_HOME}/docker/config_vault/.env.DEV')
 
 ECHO_VAR = os.environ.get('ECHO_VAR')
 
+GIT_COMMITID = subprocess.check_output(["git", 'rev-parse', 'HEAD']).strip().decode()
+
 print(f'APP_HOME={APP_HOME}')
 print(f'ECHO_VAR={ECHO_VAR}')
+print(f'GIT_COMMITID={GIT_COMMITID}')
